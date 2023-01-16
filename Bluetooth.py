24e8cabc3a315f48fa3faf33852846a2b3e48b92
Teofil007 <115471670+Teofil007@users.noreply.github.com>
2023-01-16 14:40:37 +0100

    Update Bluetooth.py

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,1 +1,20 @@
-print('heloe)
+from machine import UART, Pin
+import time
+
+uart1 = UART(0, baudrate=38400, tx=Pin(0), rx=Pin(1))
+str2 = "lo"
+
+while 1:
+    uart1.write('sa')
+    
+    if uart1.any() >0:
+        strBT = str(uart1.readline(), "utf-8")
+        print(strBT)
+        strSplit = strBT.split(";")
+        for x in range(len(strSplit)):
+            print(strSplit[x])
+            
+            
+    time.sleep(1)
+        
+    
