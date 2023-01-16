edba0704d185d43dde6bd7f18cbfdc77b1a929b6
Teofil007 <115471670+Teofil007@users.noreply.github.com>
2023-01-16 14:50:22 +0100

    Update Oled.py

--- Oled.py
+++ Oled.py
@@ -1,1 +1,12 @@
 
+from SSD1306 import SSD1306_I2C
+from machine import *
+led = Pin(26, Pin.OUT)
+led.on()
+m = 'led'
+i2c_2 = I2C(0,scl=Pin(17), sda=Pin(16), freq=400000) 
+d = SSD1306_I2C(128,64,i2c_2)
+d.fill(0)
+
+d.text("Hello Pi Pico",12,30)
+d.show()
